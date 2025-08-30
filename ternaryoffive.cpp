41f89373340ee9dc40a678dfce93440f940eedb0
Jose-Prince-Barwa <josebarwa2020@gmail.com>
2025-08-30 23:19:19 +0530

    largest among give numbers using ternary operator

--- ternaryoffive.cpp
+++ ternaryoffive.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 int main()
 {
+    int num1,num2,num3,num4,num5;
+    cout<<"\nEnter 5 numbers"<<endl;
+    cin>>num1>>num2>>num3>>num4>>num5;
+    (num1>num2)?((num1>num3)?(num1>num4)?(num1>num5)?cout<<num1:cout<<num5  
+    :(num4>num5)?cout<<num4:cout<<num5  
+    :(num3>num4)?(num3>num5)?cout<<num3:cout<<num5  :(num4>num5)?cout<<num4:cout<<num5)        
     
+ :  ((num2>num3)?(num2>num4)?(num2>num5)?cout<<num2:cout<<num5 
+ :(num4>num5)?cout<<num4:cout<<num5 
+ :(num3>num4)?(num3>num5)?cout<<num3:cout<<num5
+ :(num4>num5)?cout<<num4:cout<<num5);
 }
